7c2bcf74ec27ed27dd5af4dffa1cb762066d9664
George Chakhidze <0xfeeddeadbeef@gmail.com>
2022-02-27 19:55:51 +0400

    Initial commit

---
+++ src/SqlClientOnLinux/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SqlClientOnLinux;
+
+using System;
+using Microsoft.Data.SqlClient;
+
+public static class Program
+{
+    public static void Main()
+    {
+        using var conn = new SqlConnection("Server=lpc:(local);Integrated Security=True");
+        using var comm = conn.CreateCommand();
+
+        comm.CommandText = "SELECT 1 AS [Test];";
+
+        conn.Open();
+        Console.WriteLine(comm.ExecuteScalar());
+    }
+}
